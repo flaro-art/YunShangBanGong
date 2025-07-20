912881e120104ab579ad94476efe9a72fb2d8dcb
Gui <guiliu7788@outlook.com>
2025-07-20 17:08:37 +0800

    菜单管理的实现

--- service-oa/src/main/java/com/atguigu/auth/service/SysMenuService.java
+++ service-oa/src/main/java/com/atguigu/auth/service/SysMenuService.java
@@ -2,6 +2,7 @@ package com.atguigu.auth.service;
 
 
 import com.atguigu.model.system.SysMenu;
+import com.atguigu.vo.system.AssginMenuVo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 import java.util.List;
@@ -18,4 +19,8 @@ public interface SysMenuService extends IService<SysMenu> {
 
     List<SysMenu> findNodes();
     void removeMenuById(Long id);
+
+    List<SysMenu> findMenuByRoleId(Long roleId);
+
+    void doAssign(AssginMenuVo assignMenuVo);
 }
--- service-oa/src/main/java/com/atguigu/auth/service/impl/SysMenuServiceImpl.java
+++ service-oa/src/main/java/com/atguigu/auth/service/impl/SysMenuServiceImpl.java
@@ -1,14 +1,20 @@
 package com.atguigu.auth.service.impl;
 
+import com.atguigu.auth.service.SysRoleMenuService;
 import com.atguigu.auth.utils.MenuHelper;
 import com.atguigu.model.system.SysMenu;
 import com.atguigu.auth.mapper.SysMenuMapper;
 import com.atguigu.auth.service.SysMenuService;
+import com.atguigu.model.system.SysRoleMenu;
+import com.atguigu.vo.system.AssginMenuVo;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -20,6 +26,8 @@ import java.util.List;
  */
 @Service
 public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> implements SysMenuService {
+    @Autowired
+    private SysRoleMenuService sysRoleMenuService;
 
     @Override
     public List<SysMenu> findNodes() {
@@ -39,4 +47,46 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
         baseMapper.deleteById(id);
     }
 
+    @Override
+    public List<SysMenu> findMenuByRoleId(Long roleId) {
+        // 1 查询所有菜单_添加条件 status=1
+        LambdaQueryWrapper<SysMenu> sysMenuLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        sysMenuLambdaQueryWrapper.eq(SysMenu::getStatus, 1);
+        List<SysMenu> allSysMenuList = baseMapper.selectList(sysMenuLambdaQueryWrapper);
+
+        // 2 根据角色id，查询角色菜单表，返回角色id对应的菜单id
+        LambdaQueryWrapper<SysRoleMenu> sysRoleMenuLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        sysRoleMenuLambdaQueryWrapper.eq(SysRoleMenu::getRoleId, roleId);
+        List<SysRoleMenu> sysRoleMenuList = sysRoleMenuService.list(sysRoleMenuLambdaQueryWrapper);
+
+        // 3 根据菜单id获取对应的菜单对象
+        List<Long> menuIdList = sysRoleMenuList.stream().map(SysRoleMenu::getMenuId).collect(Collectors.toList());
+        allSysMenuList.forEach(items -> {
+            if (menuIdList.contains(items.getId())){
+                items.setSelect(true);
+            }
+        });
+
+        // 4 返回菜单树形结构
+        return MenuHelper.buildTree(allSysMenuList);
+
+    }
+
+    @Override
+    public void doAssign(AssginMenuVo assignMenuVo) {
+        LambdaQueryWrapper<SysRoleMenu> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(SysRoleMenu::getRoleId,assignMenuVo.getRoleId());
+        sysRoleMenuService.remove(wrapper);
+
+        List<Long> menuIdList = assignMenuVo.getMenuIdList();
+        for (Long menuId : menuIdList) {
+            if (StringUtils.isEmpty(menuId)){
+                continue;
+            }
+            SysRoleMenu sysRoleMenu = new SysRoleMenu();
+            sysRoleMenu.setRoleId(assignMenuVo.getRoleId());
+            sysRoleMenu.setMenuId(menuId);
+            sysRoleMenuService.save(sysRoleMenu);
+        }
+    }
 }
